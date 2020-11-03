bea6248e80617b5aa874127bb44b5523e6c279c5
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2020-11-03 20:33:35 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className='py-1'>
+        <h1 className='text-5xl text-center text-accent-1'>
+          Cédric FAMIBELLE
+        </h1>
+      </div>
+    </div>
+  )
+}
