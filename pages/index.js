a8e8a5f51b32450ac9f8cf3f4bf069102bf9dd26
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2020-11-15 02:18:17 +0100

    Index page completion

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,48 @@
-import Nav from '../components/nav'
+import fs from 'fs'
+import path from 'path'
+import PropTypes from 'prop-types'
 
-export default function IndexPage() {
+import Layout from '../components/layout'
+import Projects from '../components/projects/projects'
+
+export default function IndexPage({projects}) {
   return (
-    <div>
-      <Nav />
-      <div className='py-1'>
-        <h1 className='text-5xl text-center text-accent-1'>
-          Cédric FAMIBELLE
-        </h1>
+    <Layout>
+      <div className='lg:text-center'>
+        <h3 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl sm:leading-10'>
+          Liste des projets réalisés
+        </h3>
+        <p className='mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto'>
+          Pour chacun d’entre eux, il y a une vidéo, la documentation ou le code source.
+        </p>
       </div>
-    </div>
+      <div className='mt-10 container mx-auto'>
+        <ul className='md:grid md:grid-cols-2 xl:grid-cols-3 md:gap-x-8 md:gap-y-10'>
+          {projects.map(project => <Projects key={project.id} project={project} />)}
+        </ul>
+      </div>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const projectsDirectory = path.join(process.cwd(), 'lib')
+  const [filename] = fs.readdirSync(projectsDirectory)
+  const filePath = path.join(projectsDirectory, filename)
+  const rawContents = fs.readFileSync(filePath, 'utf8')
+  const projects = JSON.parse(rawContents)
+
+  return {
+    props: {
+      projects
+    }
+  }
+}
+
+IndexPage.propTypes = {
+  projects: PropTypes.array
+}
+
+IndexPage.defaultProps = {
+  projects: null
 }
