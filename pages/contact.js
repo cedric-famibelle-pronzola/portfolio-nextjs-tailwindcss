2dfd6ae990d2c29a8db990d400fa36c1d96981c7
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2020-11-15 02:18:28 +0100

    Create contact page

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+
+export default function Contact() {
+  return (
+    <Layout>
+      <div className='text-white'>
+        Contact
+      </div>
+    </Layout>
+  )
+}
