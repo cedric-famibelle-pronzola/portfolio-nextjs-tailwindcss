64db2a7f31750da1ca88be0a59f3396b78db637a
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2020-11-24 08:55:14 +0100

    Add express, swr & create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const next = require('next')
+const {sendMail} = require('./lib/sendmail')
+
+const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const app = express()
+  app.use(express.json()) // for parsing application/json
+
+  app.post('/send-email', (req, res) => {
+    const {body} = req
+    sendMail(body)
+    res.send('ok email envoyé')
+  })
+
+  app.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  app.listen(port, err => {
+    if (err) {
+      throw err
+    }
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
