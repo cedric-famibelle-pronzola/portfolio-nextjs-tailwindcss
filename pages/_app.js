bea66abe4d7fb463b7b9b9e959118bef0858c86b
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2020-11-25 23:28:01 +0100

    Add Context to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
+import React, {useEffect, useState} from 'react'
+
 import '../styles/index.css'
 
+export const AppContext = React.createContext()
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isDarkOn, setIsDarkOn] = useState(false)
+  const [selectedTab, setSelectedTab] = useState(null)
+
+  useEffect(() => {
+    if (isDarkOn) {
+      document.querySelector('html').classList.add('dark')
+    } else {
+      document.querySelector('html').classList.remove('dark')
+    }
+  }, [isDarkOn])
+
+  return (
+    <AppContext.Provider value={{
+      selectedTab,
+      setSelectedTab,
+      isDarkOn,
+      setIsDarkOn
+    }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+
+  ) 
 }
 
 export default MyApp
